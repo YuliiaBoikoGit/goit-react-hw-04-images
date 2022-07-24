bcf9009017fa83678dedfa43c5a52d6a3baa747f
Yuliia Boiko <littlehappinies@gmail.com>
2022-07-24 11:09:55 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,10 @@
+import PropTypes from "prop-types";
 import { Button } from "./Button.styled";
 
 export const LoadButton = ({onClick}) => {
     return <Button type="button" onClick={onClick}>Load more</Button>
+};
+
+LoadButton.propTypes = {
+    onClick: PropTypes.func.isRequired,
 };
